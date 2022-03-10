7e87525b98ac645ee107dca22dc99b2cbff1ef75
jeffallen18 <47375857+jeffallen18@users.noreply.github.com>
2022-03-09 19:57:16 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+$("button").click(function(){
+  $("h1").text("RAHHHHH GET BACK TO WORK");
+  $("button").hide();
+  $("img").attr('src', 'https://scontent.fapa1-2.fna.fbcdn.net/v/t1.6435-9/101126330_249647906306037_1656279985092034560_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=1tWvb73LefAAX9D_den&_nc_ht=scontent.fapa1-2.fna&oh=00_AT_bmYtf_hAezbN0jiURombdO_phEuccfAyfR8GpVpVy-w&oe=624DFB8E').addClass("big");
+  $("body").addClass('colorChange');
+});
